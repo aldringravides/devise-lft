d3a2b1ab5048cccc1effad108efb42a8cd0a7859
Aldrin Gravides <gravides.dren@gmail.com>
2017-07-18 13:06:44 +0800

    Copied from http://www.artechspot.com/blog/customize-devise-sing-in-and-sign-up-to-respond-in-json

--- app/controllers/devise/sessions_controller.rb
+++ app/controllers/devise/sessions_controller.rb
@@ -13,14 +13,23 @@ class Devise::SessionsController < DeviseController
   end
 
   # POST /resource/sign_in
+#   def create
+#     self.resource = warden.authenticate!(auth_options)
+#     set_flash_message(:notice, :signed_in) if is_flashing_format?
+#     sign_in(resource_name, resource)
+#     yield resource if block_given?
+#     respond_with resource, location: after_sign_in_path_for(resource) do |format|
+#       puts "Aldrin Gravides"
+#       format.json {render :json => {status: true, data: resource} } # this code will get executed for json request
+#     end
+#   end
+  
   def create
     self.resource = warden.authenticate!(auth_options)
     set_flash_message(:notice, :signed_in) if is_flashing_format?
     sign_in(resource_name, resource)
-    yield resource if block_given?
-    respond_with resource, location: after_sign_in_path_for(resource) do |format|
-      puts "Aldrin Gravides"
-      format.json {render :json => {status: true, data: resource} } # this code will get executed for json request
+    respond_with(resource) do |format|
+      format.json { render json: {redirect_url: after_sign_in_path_for(resource)}, status: 200 }
     end
   end
 
