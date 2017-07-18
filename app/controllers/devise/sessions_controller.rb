f7ec53e62c4ba6f64a8a4cd38ae53ce7167a02c4
Aldrin Gravides <gravides.dren@gmail.com>
2017-07-18 12:56:48 +0800

    Revert again the sessions_controller

--- app/controllers/devise/sessions_controller.rb
+++ app/controllers/devise/sessions_controller.rb
@@ -18,7 +18,10 @@ class Devise::SessionsController < DeviseController
     set_flash_message(:notice, :signed_in) if is_flashing_format?
     sign_in(resource_name, resource)
     yield resource if block_given?
-    respond_with resource, location: after_sign_in_path_for(resource)
+    respond_with resource, location: after_sign_in_path_for(resource) do |format|
+      puts "Aldrin Gravides"
+      format.json {render :json => {status: true, data: resource} } # this code will get executed for json request
+    end
   end
 
   # DELETE /resource/sign_out
