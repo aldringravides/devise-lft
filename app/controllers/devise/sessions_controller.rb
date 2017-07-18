33f790babe8601c7a42418ee6290d56d04ad2de9
Aldrin Gravides <gravides.dren@gmail.com>
2017-07-18 13:43:20 +0800

    Update error

--- app/controllers/devise/sessions_controller.rb
+++ app/controllers/devise/sessions_controller.rb
@@ -1,6 +1,6 @@
 class Devise::SessionsController < DeviseController
   
-  respond_to [:html, :json]
+  respond_to :html, :json
   
   prepend_before_filter :require_no_authentication, only: [:new, :create]
   prepend_before_filter :allow_params_authentication!, only: :create
