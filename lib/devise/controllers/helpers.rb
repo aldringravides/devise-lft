2b8ba460093f594aa0dfd6dc1afa950da34203bc
Aldrin Gravides <gravides.dren@gmail.com>
2017-07-18 16:15:31 +0800

    Update helpers.rb

--- lib/devise/controllers/helpers.rb
+++ lib/devise/controllers/helpers.rb
@@ -215,6 +215,7 @@ module Devise
       #   end
       #
       def after_sign_in_path_for(resource_or_scope)
+        puts "<<<<<<<<< this #{resource_or_scope}"
         stored_location_for(resource_or_scope) || signed_in_root_path(resource_or_scope)
       end
 
